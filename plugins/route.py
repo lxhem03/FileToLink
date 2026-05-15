a0c519ff7b06f0e66cd23f71ba18866ede952819
Levhcwm21. <alexags1983@gmail.com>
2026-05-15 23:39:41 +0530

    Add files via upload

--- plugins/route.py
+++ plugins/route.py
@@ -387,7 +387,7 @@ async def media_streamer(request: web.Request, id: int, secure_hash: str):
 
     mime_type = file_id.mime_type
     file_name = file_id.file_name
-    disposition = "inline"
+    disposition = "attachment"
 
     if mime_type:
         if not file_name:
